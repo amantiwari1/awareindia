8b5a9c18c2b5f9245bc068d822cac7a5c12cbcaa
aman tiwari <amantiwari78632@gmail.com>
2021-03-29 20:32:45 +0530

    remove console

--- server/server.ts
+++ server/server.ts
@@ -4,16 +4,15 @@ import app from "./app";
  * Start Express server.
  */
 
-console.log(app.get("server_port"));
 
 const server = app.listen(app.get("server_port"), () => {
-    console.log(
-        "  App is running at %s:%d in %s mode", 
-        app.get("origin_uri"),
-        app.get("server_port"),
-        app.get("env")
-    );
-    console.log("  Press CTRL-C to stop\n");
+  console.log(
+    "  App is running at %s:%d in %s mode",
+    app.get("origin_uri"),
+    app.get("server_port"),
+    app.get("env")
+  );
+  console.log("  Press CTRL-C to stop\n");
 });
 
 export default server;
