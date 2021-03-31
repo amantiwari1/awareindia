e456c86d8cd4f787e74429fb4c87e98c710be9f5
aman tiwari <amantiwari78632@gmail.com>
2021-03-31 23:05:53 +0530

    details for people card

---
+++ client/src/website/components/PeopleCard/Details.tsx
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line
+import tw from 'twin.macro'
+import {DetailsInterface} from "./PeopleInterface"
+
+const Details = ( {name, value} : DetailsInterface) => {
+    return (
+        <div tw="flex text-sm" >
+            <p>{name} :</p><p> {value}</p>
+        </div>
+    )
+}
+
+export default Details
