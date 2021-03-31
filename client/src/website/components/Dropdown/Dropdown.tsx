f53a80981c71d1c0abe4e64f7ef46ae43ceeafe3
aman tiwari <amantiwari78632@gmail.com>
2021-03-31 23:02:23 +0530

    added fully dropdown components

--- client/src/website/components/Dropdown/Dropdown.tsx
+++ client/src/website/components/Dropdown/Dropdown.tsx
@@ -1,37 +1,68 @@
 /** @jsxImportSource @emotion/react */
-import { useState } from 'react'
+// eslint-disable-next-line
 import tw from 'twin.macro'
+import { useCallback, useEffect, useRef } from 'react'
+import { AiFillCaretDown, AiFillCaretUp } from 'react-icons/ai'
+import { useDetectOutsideClick } from './useDetectOutsideClick';
+import { Link } from 'react-router-dom';
+import { DropdownInterface } from './DropdownInterface';
+import { JSX } from '@emotion/react/jsx-runtime';
 
 
-const Dropdown = () => {
-    const [Toogle, setToogle] = useState(false)
+const DropdownTwin = ({ name, navitems, onTurnNav }: DropdownInterface): JSX.Element => {
+  const dropdownRef = useRef(null);
+  const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
+  const onClick = () => {
+    setIsActive(!isActive)
 
-    return (
-        <div>
-            <div tw="relative inline-block text-left">
-                <div>
-                    <button onClick={() => setToogle(!Toogle)} type="button" tw="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="options-menu" aria-expanded="true" aria-haspopup="true">
-                        Options
-      <svg tw="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                        </svg>
-                    </button>
-                </div>
+  };
 
-                {Toogle &&
-                    <div tw="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                        <div tw="py-1" role="none">
-                            <a href="/" tw="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Account settings</a>
-                            <a href="/" tw="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Support</a>
-                            <a href="/" tw="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">License</a>
-                            <form method="POST" action="#" role="none">
-                                <button type="submit" tw="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Sign out</button>
-                            </form>
-                        </div>
-                    </div>}
-            </div>
+  const keyPress = useCallback(
+    e => {
+      if (e.key === 'Escape' && isActive) {
+        setIsActive(false);
+        console.log('I pressed');
+      }
+    },
+    [setIsActive, isActive]
+  );
+
+  useEffect(
+    () => {
+      document.addEventListener('keydown', keyPress);
+      return () => document.removeEventListener('keydown', keyPress);
+    },
+    [keyPress]
+  );
+
+  return (
+    <div>
+      <div tw="relative inline-block text-left">
+        <div >
+          <button onClick={onClick} type="button" tw="inline-flex justify-center w-full rounded-md  shadow-sm px-2 py-2  text-sm font-medium text-white hover:text-blue-400 focus:(outline-none text-blue-400) " id="options-menu" aria-expanded="true" aria-haspopup="true">
+            {name}
+            {isActive ? <AiFillCaretDown tw="ml-1 items-center mt-0.5" /> : <AiFillCaretUp tw="ml-1 items-center mt-0.5" />
+
+            }
+          </button>
         </div>
-    )
+
+        {isActive &&
+          <div ref={dropdownRef} tw=" z-20 origin-bottom-right absolute right-0 mt-1 w-36 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none dark:(bg-darkmode2 border-blue-50)" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+
+            {
+              navitems.map((navitem) => (
+                <div tw="py-1" role="none">
+
+                  <Link onClick={() => { onClick(); onTurnNav() }} to={navitem.to} tw="block px-4 py-2 text-sm text-gray-700 hover:(bg-blue-700 text-white) dark:(text-white hover:(bg-blue-700 text-white))" role="menuitem">{navitem.name}</Link>
+                </div>
+              ))
+            }
+
+          </div>}
+      </div>
+    </div>
+  )
 }
 
-export default Dropdown
+export default DropdownTwin
