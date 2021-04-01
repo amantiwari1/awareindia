56017d876fa99cce75b576dc186a13b20a291a18
aman tiwari <amantiwari78632@gmail.com>
2021-04-01 22:12:26 +0530

    added new page

---
+++ client/src/website/pages/ListofWebsite.tsx
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line
+import tw from 'twin.macro'
+import WebsiteCard from '../components/WebsiteCard/WebsiteCard'
+
+const ListofWebsite = () => {
+    return (
+        <div tw="container mx-auto" >
+            <WebsiteCard />
+        </div>
+    )
+}
+
+export default ListofWebsite
