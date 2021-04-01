e469127815ffe076b0554d171e428fe718738049
aman tiwari <amantiwari78632@gmail.com>
2021-04-01 22:13:31 +0530

    added all people data

--- client/src/website/pages/PeopleRank.tsx
+++ client/src/website/pages/PeopleRank.tsx
@@ -3,14 +3,18 @@ import React from 'react'
 // eslint-disable-next-line
 import tw from 'twin.macro'
 import {PeopleCard} from '../components'
+import PeopleData from "../../data/people.json"
 
 const PeopleRank = () => {
     return (
         <div tw="container mx-auto mt-3 grid sm:(grid-cols-2) md:(grid-cols-3) lg:(grid-cols-4) xl:(grid-cols-5) 2xl:(grid-cols-6) gap-2">
-            <PeopleCard />
-            <PeopleCard />
-            <PeopleCard />
-            <PeopleCard />
+
+            {
+            PeopleData.map((people) => (
+                <PeopleCard name={people.name} imageUrl={people.image}  job={people.job} />
+            ))
+            }
+         
         </div>
     )
 }
