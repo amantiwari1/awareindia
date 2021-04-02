4d32ec52de7f2b2d02a705ea90fca67d661e3791
aman tiwari <amantiwari78632@gmail.com>
2021-04-02 22:47:02 +0530

    styling website card

--- client/src/website/components/WebsiteCard/WebsiteCard.tsx
+++ client/src/website/components/WebsiteCard/WebsiteCard.tsx
@@ -1,11 +1,13 @@
 /** @jsxImportSource @emotion/react */
 // eslint-disable-next-line
 import tw from 'twin.macro'
+import { WebsiteCardInterface } from './WebsiteCardInferface'
 
-const WebsiteCard = () => {
+const WebsiteCard = ( {  state , website} : WebsiteCardInterface ) : JSX.Element => {
     return (
-        <div  >
-            
+        <div tw="rounded-xl flex mx-auto p-5 bg-gray-50 w-full justify-between shadow-md hover:(shadow-lg) dark:(bg-darkmode2)" >
+            <p>{state}</p>
+            <a href={website} target="_blank" rel="noreferrer" >View Website </a>
         </div>
     )
 }
